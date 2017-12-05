6873ad9a457b9d3e25db6676d38395b3a3b4e295
Andreas Klebinger <klebinger.andreas@gmx.at>
2017-12-05 23:07:20 +0100

    Added a integer based pattern

--- src/Main.hs
+++ src/Main.hs
@@ -12,11 +12,17 @@ import Criterion.Main
 
 enumList :: IO [(T,T,T)]
 enumList = do
-  let allValues = concat . replicate 10000 $ [(toEnum x,toEnum y,toEnum z) | !x <- [0..4], y <- [0..4], z <- [0..4] ] :: [(T,T,T)]
+  let allValues = concat . replicate 5000 $ [(toEnum x,toEnum y,toEnum z) | !x <- [0..4], y <- [0..4], z <- [0..4] ] :: [(T,T,T)]
   return allValues
 
-main = defaultMain [
-    env enumList $ \ args -> bgroup "main" [
+intTuples :: IO [(Int,Int,Int)]
+intTuples = do
+  let allValues = concat . replicate 5000 $ [(x,y,z) | !x <- [1..5], y <- [1..5], z <- [1..5] ]
+  return allValues
+
+main = defaultMain
+  [
+    env enumList $ \ args -> bgroup "enum1" [
       bgroup "f1" [
         bench "aug " $ nf (sum . map (\(a,b,c) -> (Aug.f1  a b c))) args
       , bench "tree" $ nf (sum . map (\(a,b,c) -> (Tree.f1 a b c))) args
@@ -24,5 +30,15 @@ main = defaultMain [
       bgroup "f2" [
         bench "aug " $ nf (sum . map (\(a,b,c) -> (Aug.f2  a b c))) args
       , bench "tree" $ nf (sum . map (\(a,b,c) -> (Tree.f2 a b c))) args
+      ]],
+    env intTuples $ \ args -> bgroup "ints" [
+      bgroup "f1Int" [
+        bench "aug " $ nf (sum . map (\(a,b,c) -> (Aug.f1Int  a b c))) args
+      , bench "tree" $ nf (sum . map (\(a,b,c) -> (Tree.f1Int a b c))) args
+      ],
+      bgroup "f2Int" [
+        bench "aug " $ nf (sum . map (\(a,b,c) -> (Aug.f2Int  a b c))) args
+      , bench "tree" $ nf (sum . map (\(a,b,c) -> (Tree.f2Int a b c))) args
       ]
- ] ]
+    ]
+  ]
--- src/Tree.hs
+++ src/Tree.hs
@@ -4,6 +4,7 @@ module Tree where
 
 import Types
 
+{-# NOINLINE f1 #-}
 f1 :: T -> T -> T -> Int
 f1 _ A E = 1
 f1 B _ _ = 2
@@ -13,6 +14,7 @@ f1 _ C B = 7
 f1 A E A = 8
 f1 _ _ _ = 5
 
+{-# NOINLINE f2 #-}
 f2 :: T -> T -> T -> Int
 f2 C A E = 1
 f2 B D _ = 2
@@ -22,3 +24,22 @@ f2 _ C B = 5
 f2 A _ A = 6
 f2 _ _ _ = 7
 
+{-# NOINLINE f1Int #-}
+f1Int :: Int -> Int -> Int -> Int
+f1Int _ 1 5 = 1
+f1Int 2 _ _ = 2
+f1Int 3 _ 3 = 3
+f1Int 4 _ _ = 4
+f1Int _ 3 2 = 7
+f1Int 1 5 1 = 8
+f1Int _ _ _ = 5
+
+{-# NOINLINE f2Int #-}
+f2Int :: Int -> Int -> Int -> Int
+f2Int 3 1 5 = 1
+f2Int 2 4 _ = 2
+f2Int 2 _ 3 = 3
+f2Int 4 _ _ = 4
+f2Int _ 3 2 = 5
+f2Int 1 _ 1 = 6
+f2Int _ _ _ = 7
