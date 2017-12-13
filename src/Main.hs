a99143d799367cc5b7a63475446d3e3ed47adf55
Andreas Klebinger <klebinger.andreas@gmx.at>
2017-12-13 09:42:21 +0100

    wip

--- src/Main.hs
+++ src/Main.hs
@@ -25,29 +25,33 @@ tuple3 [x,y,z] = (x,y,z)
 
 enumList :: IO [(T,T,T)]
 enumList = do
-  let allValues = take 1 . concat . permutations $ [(toEnum x,toEnum y,toEnum z) | !x <- [0..4], y <- [0..4], z <- [0..4] ] :: [(T,T,T)]
+  let allValues = take 100000 . concat . permutations $ [(toEnum x,toEnum y,toEnum z) | !x <- [0..4], y <- [0..4], z <- [0..4] ] :: [(T,T,T)]
   return allValues
 
 enumListRandom :: IO [(T,T,T)]
 enumListRandom = do
   setStdGen $ mkStdGen 0
-  let vals = replicateM 500 $ sequence [randomIO, randomIO, randomIO] :: IO [[T]]
+  let vals = replicateM 100000 $ sequence [randomIO, randomIO, randomIO] :: IO [[T]]
   map tuple3 <$> vals
 
 intTuples :: IO [(Int,Int,Int)]
 intTuples = do
-  let allValues = take 1 . concat . permutations $ [(x,y,z) | !x <- [1..5], y <- [1..5], z <- [1..5] ]
+  let allValues = take 100000 . concat . permutations $ [(x,y,z) | !x <- [1..5], y <- [1..5], z <- [1..5] ]
   return allValues
 
 intTuplesRandom :: IO [(Int,Int,Int)] 
 intTuplesRandom = do
   setStdGen $ mkStdGen 0
-  let vals = replicateM 500 $ sequence [randomRIO (1,5), randomRIO (1,5), randomRIO (1,5)] :: IO [[Int]]
+  let vals = replicateM 100000 $ sequence [randomRIO (1,5), randomRIO (1,5), randomRIO (1,5)] :: IO [[Int]]
   map tuple3 <$> vals
 
 enumListABC :: IO [(T,T,T)]
 enumListABC = do
-  let allValues = take 1 . concat . permutations $ [(toEnum x,toEnum y,toEnum z) | !x <- [0..2], y <- [0..2], z <- [0..2] ] :: [(T,T,T)]
+  {- 
+  While the list comprehension covers all possibilities this is not enough as it is unreasonably friendly
+  to the branch predictor when. Ideally we would use shuffle (not permutations) but for now this will do.
+  -}
+  let allValues = take 100000 . concat . permutations $ [(toEnum x,toEnum y,toEnum z) | !x <- [0..2], y <- [0..2], z <- [0..2] ] :: [(T,T,T)]
   return allValues
 
 
@@ -57,43 +61,43 @@ main =
   defaultMain
     [ genGrp enumListABC
     , benchBase
-    , env enumList $ \ args -> bgroup "enum/ordered" [
+    , bgroup "enum/ordered" [
         bgroup "f1" [
-          bench "aug " $ nf (sum . map (\(a,b,c) -> (Aug.f1  a b c))) args
-        , bench "tree" $ nf (sum . map (\(a,b,c) -> (Tree.f1 a b c))) args
+          env enumList $ \ args -> bench "aug " $ nf (sum . map (\(a,b,c) -> (Aug.f1  a b c))) args
+        , env enumList $ \ args -> bench "tree" $ nf (sum . map (\(a,b,c) -> (Tree.f1 a b c))) args
         ],
         bgroup "f2" [
-          bench "aug " $ nf (sum . map (\(a,b,c) -> (Aug.f2  a b c))) args
-        , bench "tree" $ nf (sum . map (\(a,b,c) -> (Tree.f2 a b c))) args
+          env enumList $ \ args -> bench "aug " $ nf (sum . map (\(a,b,c) -> (Aug.f2  a b c))) args
+        , env enumList $ \ args -> bench "tree" $ nf (sum . map (\(a,b,c) -> (Tree.f2 a b c))) args
         ]],
-      env intTuples $ \ args -> bgroup "ints/ordered" [
+      bgroup "ints/ordered" [
         bgroup "f1Int" [
-          bench "aug " $ nf (sum . map (\(a,b,c) -> (Aug.f1Int  a b c))) args
-        , bench "tree" $ nf (sum . map (\(a,b,c) -> (Tree.f1Int a b c))) args
+          env intTuples $ \ args -> bench "aug " $ nf (sum . map (\(a,b,c) -> (Aug.f1Int  a b c))) args
+        , env intTuples $ \ args -> bench "tree" $ nf (sum . map (\(a,b,c) -> (Tree.f1Int a b c))) args
         ],
         bgroup "f2Int" [
-          bench "aug " $ nf (sum . map (\(a,b,c) -> (Aug.f2Int  a b c))) args
-        , bench "tree" $ nf (sum . map (\(a,b,c) -> (Tree.f2Int a b c))) args
+          env intTuples $ \ args -> bench "aug " $ nf (sum . map (\(a,b,c) -> (Aug.f2Int  a b c))) args
+        , env intTuples $ \ args -> bench "tree" $ nf (sum . map (\(a,b,c) -> (Tree.f2Int a b c))) args
         ]
       ],
 
-      env enumListRandom $ \ args -> bgroup "enum/random" [
+      bgroup "enum/random" [
         bgroup "f1" [
-          bench "aug " $ nf (sum . map (\(a,b,c) -> (Aug.f1  a b c))) args
-        , bench "tree" $ nf (sum . map (\(a,b,c) -> (Tree.f1 a b c))) args
+          env enumListRandom $ \ args -> bench "aug " $ nf (sum . map (\(a,b,c) -> (Aug.f1  a b c))) args
+        , env enumListRandom $ \ args -> bench "tree" $ nf (sum . map (\(a,b,c) -> (Tree.f1 a b c))) args
         ],
         bgroup "f2" [
-          bench "aug " $ nf (sum . map (\(a,b,c) -> (Aug.f2  a b c))) args
-        , bench "tree" $ nf (sum . map (\(a,b,c) -> (Tree.f2 a b c))) args
+          env enumListRandom $ \ args -> bench "aug " $ nf (sum . map (\(a,b,c) -> (Aug.f2  a b c))) args
+        , env enumListRandom $ \ args -> bench "tree" $ nf (sum . map (\(a,b,c) -> (Tree.f2 a b c))) args
         ]],
-      env intTuplesRandom $ \ args -> bgroup "ints/random" [
+      bgroup "ints/random" [
         bgroup "f1Int" [
-          bench "aug " $ nf (sum . map (\(a,b,c) -> (Aug.f1Int  a b c))) args
-        , bench "tree" $ nf (sum . map (\(a,b,c) -> (Tree.f1Int a b c))) args
+          env intTuplesRandom $ \ args -> bench "aug " $ nf (sum . map (\(a,b,c) -> (Aug.f1Int  a b c))) args
+        , env intTuplesRandom $ \ args -> bench "tree" $ nf (sum . map (\(a,b,c) -> (Tree.f1Int a b c))) args
         ],
         bgroup "f2Int" [
-          bench "aug " $ nf (sum . map (\(a,b,c) -> (Aug.f2Int  a b c))) args
-        , bench "tree" $ nf (sum . map (\(a,b,c) -> (Tree.f2Int a b c))) args
+          env intTuplesRandom $ \ args -> bench "aug " $ nf (sum . map (\(a,b,c) -> (Aug.f2Int  a b c))) args
+        , env intTuplesRandom $ \ args -> bench "tree" $ nf (sum . map (\(a,b,c) -> (Tree.f2Int a b c))) args
         ]
       ]
     ]
